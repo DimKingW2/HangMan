417e5ca98684ea0fb16624df4a9ae7cd5a8ca967
Dimitri King <Dimitri.king.2022@student.lhschools.org>
2018-11-05 08:14:34 -0500

    update

--- src/HangMan.java
+++ src/HangMan.java
@@ -3,27 +3,33 @@ import java.util.Scanner;
 public class HangMan {
 
     static String secretWord;
+    static int guesses;
     public static void main(String[] args){
         Scanner input;
-        int guesses;
         //String secretWord;
         String letterGuessed;
-        drawManHanging();
-        findGuessedLetters();
-        showLettersGuessed();
-        //isLetterRight();
         String answer;
 
 
         input = new Scanner(System.in);
-        guesses = 0;
+        guesses = 15;
 
 
         System.out.println("Welcome to hangman. Player 1 enter a word, player 2 be ready to guess.");
         secretWord = input.next().toLowerCase();
-        System.out.println("Player 2, guess a letter.");
-        letterGuessed = input.next().toLowerCase();
-        char letterGueesedChar = letterGuessed.charAt(0);
+
+        do {
+            System.out.println("Player 2, guess a letter.");
+            letterGuessed = input.next().toLowerCase();
+            char letterGuessedChar = letterGuessed.charAt(0);
+            if (isLetterRight(letterGuessedChar) == true) {
+                System.out.println("Right!");
+                System.out.println(drawManHanging(guesses));
+            } else {
+                System.out.println("Wrong");
+                guesses --;
+            }
+        }while(guesses != 0);
         System.out.println("Would you like to play again? (Please only type yes or no)");
         answer = input.nextLine().toLowerCase();
 
@@ -31,103 +37,212 @@ public class HangMan {
 
     }
     public static boolean isLetterRight(char letterGuessed){
-        for(int i=0;i<secretWord.length();i++){
-            if(secretWord.charAt(i)==letterGuessed){
-
+        int correct = 0;
+        for(int i=0;i<secretWord.length();i++) {
+            if (secretWord.charAt(i) == letterGuessed) {
+                return true;
             }
         }
-        secretWord.charAt();
-        //String[] wordBroken = secretWord.split(" ");
-        return secretWord.contains(letterGuessed);
+                return(false);
+            }
 
-    }
-    public static String findGuessedLetters(String word){
-        String[] wordBroken=isLetterRight(word);
 
-    }
-    public static String showLettersGuessed(){
 
-    }
+    //public static String findGuessedLetters(String word){
+       // String[] wordBroken=isLetterRight(word);
+
+    //}
+    //public static String showLettersGuessed(){
+
+    //}
     public static String drawManHanging(int guesses) {
-        if (guesses = 15) {
-            return
+        if (guesses == 15) {
+            return("-------------------------\n" +
+                    "       |                |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
         }
-        if (guesses = 14) {
-            return
+        if (guesses == 14) {
+            return("-------------------------\n" +
+                    "       |                |\n" +
+                    "       0                |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
         }
-        if (guesses = 13) {
-            return
+        if (guesses == 13) {
+            return("-------------------------\n" +
+                    "       |                |\n" +
+                    "       0                |\n" +
+                    "       |                |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
         }
-        if (guesses = 12) {
-            return
+        if (guesses == 12) {
+            return("-------------------------\n" +
+                    "       |                |\n" +
+                    "       0                |\n" +
+                    "       |                |\n" +
+                    "       |                |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 11) {
-            return
+        if (guesses == 11) {
+            return("-------------------------\n" +
+                    "       |                |\n" +
+                    "       0                |\n" +
+                    "       |/               |\n" +
+                    "       |                |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 10) {
-            return
+        if (guesses == 10) {
+            return("-------------------------\n" +
+                    "       |                |\n" +
+                    "       0                |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 9) {
-            return ("----------------------\n         |           |\n         O           |\n                     |\n                     |" +
-                    "\n                     |\n                     |\n                     |\n                     |\n                     |"
-                    + "\n_____________________|");
+        if (guesses == 9) {
+            return("-------------------------\n" +
+                    "       |                |\n" +
+                    "       0 /              |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 8) {
-            return ("----------------------\n         |           |\n         O           |\n                     |\n                     |" +
-                    "\n                     |\n                     |\n                     |\n                     |\n                     |"
-                    + "\n_____________________|");
+        if (guesses == 8) {
+            return("-------------------------\n" +
+                    "       |                |\n" +
+                    "     \\ 0 /              |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 7) {
-            return ("----------------------\n         |           |\n         O           |\n                     |\n                     |" +
-                    "\n                     |\n                     |\n                     |\n                     |\n                     |"
-                    + "\n_____________________|");
+        if (guesses == 7) {
+            return("-------------------------\n" +
+                    "       |   _            |\n" +
+                    "     \\ 0 /              |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
             }
-        if (guesses = 6) {
-            return ("----------------------\n         |           |\n         O           |\n                     |\n                     |" +
-                    "\n                     |\n                     |\n                     |\n                     |\n                     |"
-                    + "\n_____________________|");
+        if (guesses == 6) {
+            return("-------------------------\n" +
+                    "    _  |   _            |\n" +
+                    "     \\ 0 /              |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "                        |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 5) {
-            return ("----------------------\n         |           |\n         O           |\n                     |\n                     |" +
-                    "\n                     |\n                     |\n                     |\n                     |\n                     |"
-                    + "\n_____________________|");
+        if (guesses == 5) {
+            return("-------------------------\n" +
+                    "    _  |   _            |\n" +
+                    "     \\ 0 /              |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "      /                 |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 4) {
-            return ("----------------------\n         |           |\n         O           |\n                     |\n                     |" +
-                    "\n                     |\n                     |\n                     |\n                     |\n                     |"
-                    + "\n_____________________|");
+        if (guesses == 4) {
+            return("-------------------------\n" +
+                    "    _  |   _            |\n" +
+                    "     \\ 0 /              |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "      /\\                |\n" +
+                    "                        |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 3) {
-            return ("----------------------\n         |           |\n         O           |\n                     |\n                     |" +
-                    "\n                     |\n                     |\n                     |\n                     |\n                     |"
-                    + "\n_____________________|");
+        if (guesses == 3) {
+            return("-------------------------\n" +
+                    "    _  |   _            |\n" +
+                    "     \\ 0 /              |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "      /\\                |\n" +
+                    "     /                  |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 2) {
-            return ("----------------------\n         |           |\n         O           |\n                     |\n                     |" +
-                    "\n                     |\n                     |\n                     |\n                     |\n                     |"
-                    + "\n_____________________|");
+        if (guesses == 2) {
+            return("-------------------------\n" +
+                    "    _  |   _            |\n" +
+                    "     \\ 0 /              |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "      /\\                |\n" +
+                    "     /  \\               |\n" +
+                    "                        |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 1) {
-            return ("----------------------\n         |           |\n         O           |\n                     |\n                     |" +
-                    "\n                     |\n                     |\n                     |\n                     |\n                     |"
-                    + "\n_____________________|");
+        if (guesses == 1) {
+            return("-------------------------\n" +
+                    "    _  |   _            |\n" +
+                    "     \\ 0 /              |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "      /\\                |\n" +
+                    "     /  \\               |\n" +
+                    "   _                    |\n"+
+                    "________________________|");
 
         }
-        if (guesses = 0) {
-            return ("----------------------\n         |           |\n         O           |\n                     |\n                     |" +
-                    "\n                     |\n                     |\n                     |\n                     |\n                     |"
-                    + "\n_____________________|");
+        if (guesses == 0) {
+            return("-------------------------\n" +
+                    "    _  |   _            |\n" +
+                    "     \\ 0 /              |\n" +
+                    "      \\|/               |\n" +
+                    "       |                |\n" +
+                    "      /\\                |\n" +
+                    "     /  \\               |\n" +
+                    "   _      _             |\n"+
+                    "________________________|");
 
         }
     }
